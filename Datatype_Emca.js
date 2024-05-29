fda6d5606b46067a21a2fd5a4eacf03c35a800de
Pritam-Shome <127998270+Pritam-Shome@users.noreply.github.com>
2024-05-30 02:12:01 +0530

    first commit

---
+++ Datatype_Emca.js
@@ -0,0 +1,34 @@
+"use strict";  //treat all JS code as newer version
+
+//alert(3+3) //we are using nodejs , not browser
+
+let name = "pritam" //string datatype 
+let age = 20        //number datatype
+let isLoggedIn = false // boolean datatype
+let state;  //undefined datatype
+let marks = null; //null datatype
+
+
+/*
+types of datatype
+
+number => 2 to the power 53
+bigint => for storing large number
+String => ""
+boolean => true/false
+null => standalone Value
+undefined =>  if not initialised
+symbol => unique (used in react)
+
+
+object => later study
+*/
+
+//console.log(typeof );  return the type of datatype
+
+console.log(typeof state);// it will return undefined
+console.log(typeof marks);// it will return object
+ 
+/*
+ notes: datatype of null is object..
+ */
---
+++ variable_datatype.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "pritamshome1@gmail.com" 
+var accountPassword = "12345"
+accountCity = "agartala"
+
+// accountId = 144199  //const cannot be changed
+// console.log(accountId);
+
+// Note: never use var in JS
+
+accountEmail="tapanshome.gmail.com" //let can be changed
+accountPassword = "5151555" //var can be changed
+accountCity = "kolkata"
+
+let accountstate; //it will automatically print undefined if nothing is initialized
+console.table([accountId, accountEmail, accountPassword, accountCity, accountstate])
