d1e86b0994041e2b12d657c6b9b5500775ca4f85
Pritam-Shome <127998270+Pritam-Shome@users.noreply.github.com>
2024-05-31 00:15:40 +0530

    conversion of dataype

---
+++ DataTypeConversion.js
@@ -0,0 +1,64 @@
+// let score = "33abc"
+// console.log(typeof score)
+
+// let valueinNumber =Number(score)//to convert in number
+// console.log(typeof valueinNumber)
+// console.log(valueinNumber);////IT will print NaN(not a number)
+
+
+// let s2 = null
+// console.log(typeof s2)
+
+// let v2 =Number(s2)//to convert in number
+// console.log(typeof v2)
+// console.log(v2)//it will print 0
+
+// let s2 = undefined
+// console.log(typeof s2)
+
+// let v2 =Number(s2)//to convert in number
+// console.log(typeof v2)
+// console.log(v2)//it will print NaN
+
+
+// let s2 = true
+// console.log(typeof s2)
+
+// let v2 =Number(s2)//to convert in number
+// console.log(typeof v2)
+// console.log(v2)//it will print 1
+
+
+/*
+"33"=>33
+"33abc"=> NaN
+true=>1
+false=>0
+null=>0
+undefined=>NaN
+     ----------anything to number conversion------
+*/ 
+
+
+
+//----------  --------
+// let check="pritam"
+
+// let booleanCheck=Boolean(check)
+// console.log(booleanCheck);
+
+//1=> true    0=> false
+//""=>false   "Pritam"=>true
+//     -----anything to boolean------
+
+
+
+
+// -------------------------------
+let Number = 33
+let stringNumber = String (Number);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
