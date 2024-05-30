574ecf3ee16b7ad8979270fcfd84025f03fad6d0
Pritam-Shome <127998270+Pritam-Shome@users.noreply.github.com>
2024-05-31 01:08:49 +0530

    Opertions in JS

---
+++ Operation.js
@@ -0,0 +1,26 @@
+// let value =3
+// let negValue= -value
+// console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);///this means 2 to th power 2
+// console.log(2/2);
+// console.log(2%2);
+
+
+
+
+
+// let str1="PRI"
+// let str2="TAM"
+// let str3=str1 + str2
+// console.log(str3);
+
+
+console.log("1" + 2);//output --- 12
+console.log(1+"2");//output ---12
+console.log("1"+2+2);//output ---122
+console.log(1+2+"2");//output ---32
