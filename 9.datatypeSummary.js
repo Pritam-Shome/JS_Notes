83be2226e097e9ca3af5d98de75347cc8fe2d93e
Pritam-Shome <127998270+Pritam-Shome@users.noreply.github.com>
2024-05-31 01:53:21 +0530

    JS datatype summary

---
+++ 9.datatypeSummary.js
@@ -0,0 +1,64 @@
+/*
+{//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail; //undefined
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);///false
+
+// const bigNumber = 3456543576654356754n
+}
+*/
+
+
+
+
+/*
+{
+// Reference (Non primitive)
+      
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+
+
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+//object -->    {
+
+}
+
+
+
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+function--> function(){
+
+}
+
+
+
+
+console.log(typeof anotherId);//used to return the type of datatype inside it
+///all non primitive data type returns object.
+
+
+}
+*/
+
+
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
